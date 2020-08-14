9feaeee1e638a78fe0bd554d1c54a03101406c05
amairovi <andrei.mairovich@electrolux.com>
2020-08-14 11:35:05 +0300

    Add test for bean name

--- spring/src/test/java/com/amairovi/context/BeanNaming.java
+++ spring/src/test/java/com/amairovi/context/BeanNaming.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,6 +25,19 @@ public class BeanNaming {
         assertThat(context4.getBean(ComponentClass1.class)).isInstanceOf(ComponentClass1.class);
         assertThat(context4.getBean("componentClass1")).isInstanceOf(ComponentClass1.class);
         assertThatThrownBy(() -> context4.getBean(com.amairovi.context.test1.inner_2.ComponentClass1.class)).isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+
+    @Component("another-name")
+    private static class Clazz2 {
+
+    }
+
+    @Test
+    void when_rename_bean_through_component_annotation() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Clazz2.class);
+
+        assertThat(context.getBean("another-name")).isInstanceOf(Clazz2.class);
+        assertThatThrownBy(() -> context.getBean("clazz2")).isInstanceOf(NoSuchBeanDefinitionException.class);
     }
 
 }
